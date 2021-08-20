7ac03f4a5b8cccfe2f54409eee86d5db500fae13
amine-bach <amine.bachri@epitech.eu>
2021-08-20 15:08:18 +0000

    remove pybind from bad location

--- bind.sh
+++ bind.sh
@@ -1,3 +1,3 @@
 cp ./src/*.* ./wrap
 rm ./wrap/main.cpp
-echo "All files copied from ./src to ./wrap (except main.cpp)."
+echo "All files copied from ./src to ./wrap (except main.cpp)."https://github.com/pybind/pybind11
